69aaaa63edfbeb5de7263d5d69fc83a8e5ad372f
nadiduno <nadiduno@gmail.com>
2024-03-13 17:16:58 -0300

    Config router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { HomePage } from './pages/HomePage'
+import { ContactPage } from './pages/ContactPage'
+import { LinkPage } from './pages/LinkPage'
 
 export function Router () {
   return (
         <Routes >
             <Route path='/' element={<HomePage />} />
+            <Route path='/contato' element={<ContactPage />} />
+            <Route path='/link' element={<LinkPage />} />
         </Routes>
   )
 }
