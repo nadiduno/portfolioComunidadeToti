48f72eb0dfe6047f7d09ec1772c431e6b20d5d18
nadiduno <nadiduno@gmail.com>
2024-03-12 22:46:40 -0300

    container - photo

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -3,7 +3,11 @@ import styles from './Photo.module.css'
 export function Photo () {
     return(
         <div className={styles.containerphoto}>
-            Photo
+            <img
+                className={styles.cover}
+                src="https://github.com/nadiduno/portfolioComunidadeToti/blob/main/src/assets/photoman.png?raw=true"
+                alt="Foto de Nadi Duno, Nadi é uma mulher, parda de cabelo curto, a foto esta em branco e preto, com um circulo verde neon com codigos de programação"
+            />
         </div>
     )
 }
