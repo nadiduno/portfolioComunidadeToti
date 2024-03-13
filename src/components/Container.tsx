77f1f933e5741c6ff4b99719c7cdc946f85f0b1f
nadiduno <nadiduno@gmail.com>
2024-03-12 22:34:24 -0300

    container - photo

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,11 +1,13 @@
-import style from './Container.module.css'
+import styles from './Container.module.css'
 import { PersonalDetails } from './PersonalDetails'
+import { Photo } from './Photo'
 
 export function Container () {
     return(
-        <header className={style.container}>
-            <div>
+        <header className={styles.container}>
+            <div className={styles.wrapper}>
                 <PersonalDetails />
+                <Photo />
             </div>
             <div>
                 Projeto principal
---
+++ src/components/Photo.tsx
@@ -0,0 +1,9 @@
+import styles from './Photo.module.css'
+
+export function Photo () {
+    return(
+        <div className={styles.containerphoto}>
+            Photo
+        </div>
+    )
+}
