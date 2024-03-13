85589f20ac4aab8bea38eeaa301639cbb48bd3e3
nadiduno <nadiduno@gmail.com>
2024-03-13 19:18:37 -0300

    Config router-buttom

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,24 @@
+import { NavLink } from 'react-router-dom'
+
 import styles from './Menu.module.css'
 
 export function Menu () {
     return(
         <main className={styles.menu}>
-            <span>Home</span>   
-            <span>Meus trabalhos</span>
-            <span>Contato</span>
-            <span>Link</span>
+            <nav>
+                <NavLink to='/' title='Home'>
+                    <span>Home</span>
+                </NavLink>
+                <NavLink to='/trabalho' title='meus trabalho'>
+                    <span>Meus trabalhos</span>
+                </NavLink>
+                <NavLink to='/contato' title='contato'>
+                    <span>Contato</span>
+                </NavLink>
+                <NavLink to='/link' title='Link'>
+                    <span>Link</span>
+                </NavLink>
+            </nav>
         </main>
     )
 }
