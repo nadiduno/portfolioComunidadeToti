ef782a1faa2ba9acf97fe8fc6ef1b2194e85884f
nadiduno <nadiduno@gmail.com>
2024-03-13 22:39:27 -0300

    update code

--- src/components/ProjectMain.tsx
+++ src/components/ProjectMain.tsx
@@ -1,11 +1,16 @@
+import { PowerBIEmbed } from 'powerbi-client-react';
+import { models } from 'powerbi-client';
+
 import styles from './ProjectMain.module.css'
+
 
 export function ProjectMain() {
     return(
         <>
             <div className={styles.container}>
-                Projeto principal
+                <iframe title="Vendas" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMDEwZjJkYzQtYTBmMC00ZWE3LWI5MjktZDJhZTUzODI2NjVhIiwidCI6IjE0YmU1ZTFkLTM2MGYtNDg0Ni1iNGIwLTJlNTkzNzc1NmQwZCJ9" frameborder="0" allowFullScreen="true"></iframe>                
             </div>
         </>
     )
 }
+
