bd43e1bb6c23659704e4fafea77e848e896386ab
nadiduno <nadiduno@gmail.com>
2024-03-13 23:37:58 -0300

    Embeber Dashbord Power BI

--- src/components/ProjectMain.tsx
+++ src/components/ProjectMain.tsx
@@ -1,6 +1,3 @@
-import { PowerBIEmbed } from 'powerbi-client-react';
-import { models } from 'powerbi-client';
-
 import styles from './ProjectMain.module.css'
 
 
@@ -8,7 +5,9 @@ export function ProjectMain() {
     return(
         <>
             <div className={styles.container}>
-                <iframe title="Vendas" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMDEwZjJkYzQtYTBmMC00ZWE3LWI5MjktZDJhZTUzODI2NjVhIiwidCI6IjE0YmU1ZTFkLTM2MGYtNDg0Ni1iNGIwLTJlNTkzNzc1NmQwZCJ9" frameborder="0" allowFullScreen="true"></iframe>                
+                <div className={styles.iframeborder}>   
+                    <iframe title="Vendas" width="750" height="498" src="https://app.powerbi.com/view?r=eyJrIjoiMDEwZjJkYzQtYTBmMC00ZWE3LWI5MjktZDJhZTUzODI2NjVhIiwidCI6IjE0YmU1ZTFkLTM2MGYtNDg0Ni1iNGIwLTJlNTkzNzc1NmQwZCJ9" frameborder="1" allowFullScreen="true" />
+                </div>
             </div>
         </>
     )
