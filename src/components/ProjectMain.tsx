e2c1172c7ae2d24a119b12f0a01095f05bbc1a1e
nadiduno <nadiduno@gmail.com>
2024-03-13 23:44:54 -0300

    Erro typescript Dashboard PowerBi

--- src/components/ProjectMain.tsx
+++ src/components/ProjectMain.tsx
@@ -1,12 +1,26 @@
 import styles from './ProjectMain.module.css'
 
+const PowerBiDashboard: React.FC = () => {
+    return (
+      <iframe
+        title="Vendas"
+        width="750"
+        height="498"
+        src="https://app.powerbi.com/view?r=eyJrIjoiMDEwZjJkYzQtYTBmMC00ZWE3LWI5MjktZDJhZTUzODI2NjVhIiwidCI6IjE0YmU1ZTFkLTM2MGYtNDg0Ni1iNGIwLTJlNTkzNzc1NmQwZCJ9"
+        allowFullScreen={true as boolean}
+      />
+    );
+  };
+  
 
 export function ProjectMain() {
     return(
         <>
             <div className={styles.container}>
                 <div className={styles.iframeborder}>   
-                    <iframe title="Vendas" width="750" height="498" src="https://app.powerbi.com/view?r=eyJrIjoiMDEwZjJkYzQtYTBmMC00ZWE3LWI5MjktZDJhZTUzODI2NjVhIiwidCI6IjE0YmU1ZTFkLTM2MGYtNDg0Ni1iNGIwLTJlNTkzNzc1NmQwZCJ9" frameborder="1" allowFullScreen="true" />
+                    {/* <iframe title="Vendas" width="750" height="498" src="https://app.powerbi.com/view?r=eyJrIjoiMDEwZjJkYzQtYTBmMC00ZWE3LWI5MjktZDJhZTUzODI2NjVhIiwidCI6IjE0YmU1ZTFkLTM2MGYtNDg0Ni1iNGIwLTJlNTkzNzc1NmQwZCJ9" frameborder="1" allowFullScreen="true" /> */}
+                    {/*Da erro com o typescript*/}
+                    <PowerBiDashboard />
                 </div>
             </div>
         </>
