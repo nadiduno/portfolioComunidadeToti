538d5e0f70210024cff242b6142ef064bff03714
nadiduno <nadiduno@gmail.com>
2024-03-15 13:53:18 -0300

    Link social media

--- src/components/RedesSociais.tsx
+++ src/components/RedesSociais.tsx
@@ -3,32 +3,32 @@ import './RedesSociais.css'; // Importe o arquivo CSS separado
 
 interface RedesSociaisProps { }
 
-const RedesSociais: React.FC<RedesSociaisProps> = () => {
+export const RedesSociais: React.FC<RedesSociaisProps> = () => {
     return (
         <div className="redes-portafolio">
             <ul className="list-inline redes-gremio">
                 <li>
-                    <a href="url_facebook" target="_blank" title="Ir a Facebook">
+                    <a href="https://www.facebook.com/" target="_blank" title="Ir a Facebook">
                         Facebook
                     </a>
                 </li>
                 <li>
-                    <a href="url_instagram" target="_blank" title="Ir a Instagram">
+                    <a href="https://www.instagram.com/" target="_blank" title="Ir a Instagram">
                         Instagram
                     </a>
                 </li>
                 <li>
-                    <a href="url_linkedin" target="_blank" title="Ir a Linkedin">
+                    <a href="linkedin.com/in/" target="_blank" title="Ir a Linkedin">
                         Linkedin
                     </a>
                 </li>
                 <li>
-                    <a href="url_twitter" target="_blank" title="Ir a Twitter">
+                    <a href="https://twitter.com/" target="_blank" title="Ir a Twitter">
                         Twitter
                     </a>
                 </li>
                 <li>
-                    <a href="url_youtube" target="_blank" title="Ir a Youtube">
+                    <a href="https://www.youtube.com/" target="_blank" title="Ir a Youtube">
                         Youtube
                     </a>
                 </li>
@@ -36,5 +36,3 @@ const RedesSociais: React.FC<RedesSociaisProps> = () => {
         </div>
     );
 };
-
-export default RedesSociais;
