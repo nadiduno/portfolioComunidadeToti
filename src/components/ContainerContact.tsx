848068b42998cc6775400a56e98045cf20254a38
Victor Alcala <alcalavictor2001@gmail.com>
2024-03-17 18:25:20 -0400

    update code contact

--- src/components/ContainerContact.tsx
+++ src/components/ContainerContact.tsx
@@ -1,29 +1,30 @@
 import styles from './ContainerContact.module.css'
-import {FiPhoneCall, FiMail} from 'react-icons/fi'
+import {FiMail} from 'react-icons/fi'
 
 
 export function ContainerContact () {
     return(
         <header className={styles.container}>
-            <div className={styles.content}>
-                <div className={styles.description}>
-                    <h1>NOME</h1>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt voluptas minus saepe molestiae, dolore sed vero nam quod doloremque quis rerum pariatur labore perferendis qui accusantium neque dolorem. Sapiente, quam!</p>
+            <div className={styles.description}>
+                <h2>contate-me</h2>
+                <div className={styles.nome}>
+                    <h1>roberto carlos</h1>
+                    <h3>analista de dados</h3>
                 </div>
-                <div className={styles.support}>
-                    <div className={styles.whatsapp}>
-                        <a href="https://api.whatsapp.com/send?phone=5592999999999&text=%22mensagem%22" target="_blank" title="Escribir whatsapp" rel='noopener'>
-                        <FiPhoneCall className= {styles.icons} size={25} color='fff'/>
-                        </a>
-                        <p>WHATSAPP</p>
-                    </div>
-                    <div className={styles.email}>
-                        <a href="mailto:totidiversidade@gmail.com" target="_blank" title="Escribir correo" rel='noopener'>
-                        <FiMail className= {styles.icons} size={25} color='fff'/>
-                        </a>
-                        <p>GMAIL</p>
-                    </div>
-                </div>
+            <div className={styles.comunication}>
+                <a href="mailto:" target='blank' title='Enviar email' rel='noopener'>
+                    <FiMail className='styles.icons' size={60} color='fff'/> 
+                </a>
+                <a href="" target='black' title='Enviar WhatsApp' rel='noopener'>
+                    <FiMail className='styles.icons' size={60} color='fff'/>
+                </a>
+            </div>
+            </div>
+            <div className={styles.imagenContacto}>
+                <img
+                    src="https://github.com/nadiduno/portfolioComunidadeToti/blob/main/src/assets/photoman.png?raw=true"
+                    alt="Foto de um dev, clocar desc"
+                />
             </div>
         </header>
     )
