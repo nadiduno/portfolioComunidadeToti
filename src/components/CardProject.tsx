6ef2b262e525aee771ca71e75fbb59f964e6ea27
MANUEL PEDRO BERNARDO <manuelunilab.eng@gmail.com>
2024-03-18 19:36:45 -0300

    Update Card Animation

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -3,7 +3,11 @@ import styles from './CardProject.module.css'
 export function CardProject () {
     return(
         <div className={styles.containercard}>
-            Card
+           <img
+                className={styles.cover}
+                src="https://github.com/nadiduno/portfolioComunidadeToti/blob/main/src/assets/imgs/proj1.png"
+                alt="Foto de um dev, clocar desc"
+            />  
         </div>
     )
 }
