4e60903944eb9033e50e5d2ecad7ec9439e6bef4
MANUEL PEDRO BERNARDO <manuelunilab.eng@gmail.com>
2024-03-19 01:52:34 -0300

    Update Card Animation

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -1,13 +1,23 @@
 import styles from './CardProject.module.css'
 
+
 export function CardProject () {
     return(
-        <div className={styles.containercard}>
-           <img
+
+        
+        <div className={styles.CardProject}>
+            
+            <img
+            
                 className={styles.cover}
-                src="https://github.com/nadiduno/portfolioComunidadeToti/blob/main/src/assets/imgs/proj1.png"
+                src="https://github.com/nadiduno/portfolioComunidadeToti/blob/main/src/assets/proj1.png?raw=true"
                 alt="Foto de um dev, clocar desc"
-            />  
+                
+            />   
+            <div className={styles.about}>
+               <h2>Dashboard de Vendas/ Power Bi</h2>
+                <p>Dashboard de venda é ....</p>
+             </div>               
         </div>
     )
 }
