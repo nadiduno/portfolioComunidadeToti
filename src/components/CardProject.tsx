f19925cee1b05ddddd7914d7214801a4e07076e3
MANUEL PEDRO BERNARDO <manuelunilab.eng@gmail.com>
2024-03-19 20:05:56 -0300

    Update Card Animation

--- src/components/CardProject.tsx
+++ src/components/CardProject.tsx
@@ -27,12 +27,12 @@ export function CardProject () {
             <img
             
                 className={styles.cover}
-                src="https://github.com/nadiduno/portfolioComunidadeToti/blob/main/src/assets/proj1.png?raw=true"
+                src="https://github.com/nadiduno/portfolioComunidadeToti/blob/main/src/assets/imgs/proj2.png?raw=true"
                 alt="Foto de um dev, clocar desc"
                 
             />   
             <div className={styles.about}>
-               <h2>Dashboard de Vendas/ Power Bi</h2>
+               <h2>Dashboard de RH/ Power Bi</h2>
                 <p>Dashboard de venda é ....</p>
              </div> 
              
