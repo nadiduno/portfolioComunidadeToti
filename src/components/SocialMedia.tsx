1281a069e71fef202a1e0dde2b48001cbb570531
MaolyDevTech <144358009+MaolyDevTech@users.noreply.github.com>
2024-04-01 22:10:34 +0000

    commit

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,82 +1,76 @@
-<<<<<<< HEAD:src/components/RedesSociais.tsx
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
 import { IconType } from 'react-icons';
-import {FiHome, FiGithub, FiFacebook, FiInstagram, FiLinkedin, FiYoutube} from 'react-icons/fi'
-=======
-import styles from './SocialMedia.module.css'
-import { FiInstagram, FiLinkedin, FiYoutube, FiGithub} from 'react-icons/fi';
-import { FaWhatsapp } from 'react-icons/fa'
->>>>>>> d0d5303 (Alterações de menu de links e page do links):src/components/SocialMedia.tsx
+import { FiHome, FiGithub, FiFacebook, FiInstagram, FiLinkedin, FiYoutube } from 'react-icons/fi';
+import styles from './SocialMedia.module.css';
 
-import styles from './RedesSociais.module.css'
-  
-interface IconeAppType
-{
-      id: number
-      iconemenu: IconType
-      title: string
-      urlicone: string
+interface IconeAppType {
+    id: number
+    iconemenu: IconType
+    title: string
+    urlicone: string
 }
 
 const iconesApp: IconeAppType[] = [
     {
-      id: 0,
-      iconemenu: FiHome,
-      title: 'Ir a Home',
-      urlicone: '/' 
-    }, 
+        id: 0,
+        iconemenu: FiHome,
+        title: 'Ir a Home',
+        urlicone: '/'
+    },
     {
         id: 1,
         iconemenu: FiLinkedin,
         title: 'Ir a Linkedin',
-        urlicone: 'https://www.linkedin.com/' 
-    },  
+        urlicone: 'https://www.linkedin.com/'
+    },
     {
         id: 2,
         iconemenu: FiInstagram,
         title: 'Ir a Instagram',
-        urlicone: 'https://www.instagram.com/' 
+        urlicone: 'https://www.instagram.com/'
     },
     {
         id: 3,
         iconemenu: FiFacebook,
         title: 'Ir a Facebook',
-        urlicone: 'https://www.facebook.com/' 
+        urlicone: 'https://www.facebook.com/'
     },
     {
         id: 4,
         iconemenu: FiYoutube,
         title: 'Ir a Youtube',
-        urlicone: 'https://www.youtube.com/' 
+        urlicone: 'https://www.youtube.com/'
     },
     {
         id: 5,
         iconemenu: FiGithub,
         title: 'Ir a Github',
-        urlicone: 'https://github.com/' 
-    }  
+        urlicone: 'https://github.com/'
+    }
 ]
 
-export function RedesSociais( ){
-    return( 
+export function RedesSociais() {
+    return (
         <div className={styles.redesPortafolio}>
             <ul className={styles.listinline}>
 <<<<<<< HEAD:src/components/RedesSociais.tsx
-            {iconesApp.map((iconeApp) => (
-                    <li key={iconeApp.id}> 
-                        {iconeApp.id === 0 ? (
-                            <nav>
-                                <NavLink to={iconeApp.urlicone} title={iconeApp.title} rel='noopener noreferrer'>
-                                    <iconeApp.iconemenu className={`${styles.icons} ${styles.iconColor}`} size={25} />
-                                </NavLink>
-                            </nav>
-                        ) : (
-                            <a href={iconeApp.urlicone} target="_blank" title={iconeApp.title} rel='noopener noreferrer'>
-                                <iconeApp.iconemenu className={`${styles.icons} ${styles.iconColor}`} size={25} />
-                            </a>
-                        )}
-                    </li>
-                ))}
+    {
+        iconesApp.map((iconeApp) => (
+            <li key={iconeApp.id}>
+                {iconeApp.id === 0 ? (
+                    <nav>
+                        <NavLink to={iconeApp.urlicone} title={iconeApp.title} rel='noopener noreferrer'>
+                            <iconeApp.iconemenu className={`${styles.icons} ${styles.iconColor}`} size={25} />
+                        </NavLink>
+                    </nav>
+                ) : (
+                    <a href={iconeApp.urlicone} target="_blank" title={iconeApp.title} rel='noopener noreferrer'>
+                        <iconeApp.iconemenu className={`${styles.icons} ${styles.iconColor}`} size={25} />
+                    </a>
+                )}
+            </li>
+        ))
+    }
 =======
                 <li>
                     <a href="linkedin.com/in/" target="_blank" title="Ir a Linkedin">
@@ -104,9 +98,8 @@ export function RedesSociais( ){
                     </a>
                 </li>
 >>>>>>> d0d5303 (Alterações de menu de links e page do links):src/components/SocialMedia.tsx
-            </ul>
-        </div>
+            </ul >
+        </div >
     );
 }
 
-                    
