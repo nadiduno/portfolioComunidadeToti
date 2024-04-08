67a9688167f361245223534cbc4668dae3ffe373
nadiduno <nadiduno@gmail.com>
2024-04-07 23:58:06 -0300

    Update link collaborators

--- src/components/FooterLink.tsx
+++ src/components/FooterLink.tsx
@@ -7,34 +7,37 @@ const colabsApp: ColabAppType[] = [
     {
       id: 0,
       title: 'Nadi Duno',
-      descrition: 'Mulher parda de cabelo preto, fundo amarelo',
+      descrition: 'Mulher parda aparentemente de 40 anos, camisa branca, cabelo preto, fundo amarelo',
       url: 'https://www.linkedin.com/in/nadiduno/',
-      urlphoto: 'https://media.licdn.com/dms/image/D4D35AQGGPqar_zxotg/profile-framedphoto-shrink_200_200/0/1709583111683?e=1712282400&v=beta&t=SvmKnGFpWOc3OtdkcsPG_xUFV1IijHZhNXkeaTKsWc8'
+      urlphoto: 'https://github.com/nadiduno.png'
     },
     {
         id: 1,
         title: 'Maoly Lara',
+        descrition: 'Mulher branca aparentemente de 40 anos, camisa branca e casaco, cabelo ruivo, fundo de cidade',
         url: 'https://www.linkedin.com/in/maolylara/',
-        urlphoto: 'https://media.licdn.com/dms/image/D4D03AQFxpNQeFmofnw/profile-displayphoto-shrink_200_200/0/1698188040979?e=1715817600&v=beta&t=Jt7znLWjfsUigLdD8-gvMPku1tFXmo5sf1rDPCOT-hs'
+        urlphoto: 'https://github.com/MaolyDevTech.png'
       },
       {
         id: 2,
         title: 'Manuel Bernardo',
+        descrition: 'Homem preto aparentemente de 25 anos, camisa branca e terno, cabelo cacheado preto, fundo de escritorio',
         url: 'https://www.linkedin.com/in/manuel-bernardo-165373208/',
-        urlphoto: 'https://media.licdn.com/dms/image/D4D03AQHu2DVCy1LZsg/profile-displayphoto-shrink_200_200/0/1709568363617?e=1716422400&v=beta&t=B-I0fWlxXal_FcvE1ocE8h0-EWok9mu6EvApLHQmh9g'
+        urlphoto: 'https://github.com/ManueBernardo.png'
       },
       {
         id: 3,
         title: 'Victor Alcala',
+        descrition: 'Homem pardo aparentemente de 20 anos, camisa preta, cabelo preto, fone de ouvido, fundo brano',
         url: 'https://www.linkedin.com/in/alcalamvictor/',
-        urlphoto: 'https://media.licdn.com/dms/image/D4D35AQGuQkqdaXo7BQ/profile-framedphoto-shrink_200_200/0/1710717303299?e=1712282400&v=beta&t=dPIzAL0ugfGrM1MCFFCg7iGw5AKQCbIT5G-rr97XBl0'
+        urlphoto: 'https://github.com/VictorAlcala0111.png'
       },
       {
         id: 4,
         title: 'Yurisay Martinez',
-        descrition: 'Mulher branca aparentemente com 35 anos, com camisa branca, cabelo preto, sorrindo e com pose empoderada. Está em pé em frente a uma cortina bege',
+        descrition: 'Mulher branca aparentemente com 35 anos, com camisa preta, cabelo preto, sorrindo e com pose empoderada. No fundo um morro',
         url: 'https://www.linkedin.com/in/yurisay-martinez/',
-        urlphoto: 'https://media.licdn.com/dms/image/D4D03AQEs87tKtvqYag/profile-displayphoto-shrink_200_200/0/1692373276885?e=1715817600&v=beta&t=oiPFrHx7Z5FbtIupVNjYRePxlvhMYDxV8mc18b8_D-8'
+        urlphoto: 'https://github.com/yurisaym.png'
       }
     
 ]  
